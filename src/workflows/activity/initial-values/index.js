e1d912003bc062732a26833f1aad2d23f1468a79
Muzikayise Flynn Buthelezi (zuluCoda) <muzi@mfbproject.co.za>
2020-04-20 23:20:32 +0200

    setup-phase-1: adding initi values tests

--- src/workflows/activity/initial-values/index.js
+++ src/workflows/activity/initial-values/index.js
@@ -1,3 +1,11 @@
-const initialValues = {};
+import activityConfig from '../constants';
+
+/**
+ * Initial Values
+ * @type InitialValues
+ */
+const initialValues = {
+  [activityConfig.SELECT_NAME]: activityConfig.ACTIVITY_VALUE_NONE,
+};
 
 export default initialValues;
