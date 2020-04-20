eb699865e2b9cc0d123dd9abdb93017aa98d9e6f
Muzikayise Flynn Buthelezi (zuluCoda) <muzi@mfbproject.co.za>
2020-04-20 23:18:59 +0200

    setup-phase-1: playing around with JSDoc

---
+++ src/js-doc.js
@@ -0,0 +1,20 @@
+/**
+ * @typedef Workflow
+ * @type {object}
+ * @property {React.Component} Component - React component
+ * @property {function} payload - payload function
+ * @property {function} validate - validate function
+ * @property {object} initialValues - initial values object
+ */
+
+/**
+ * @typedef InitialValues
+ * @type {object}
+ */
+
+/**
+ * @typedef Dropdown
+ * @type {object}
+ * @property {string} value
+ * @property {string} title
+ */
