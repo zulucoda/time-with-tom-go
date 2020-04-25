2dd7cd10e62c7f4d883eec2599378d558f258441
Muzikayise Flynn Buthelezi (zuluCoda) <muzi@mfbproject.co.za>
2020-04-25 20:06:49 +0200

    booking-workflow: adding initial values

---
+++ src/workflows/booking/initial-values/index.js
@@ -0,0 +1,13 @@
+import bookingConfig from '../constants';
+
+/**
+ * Initial Values
+ * @type InitialValues
+ */
+const initialValues = {
+  [bookingConfig.INPUT_DATE]: '',
+  [bookingConfig.INPUT_START_TIME]: '',
+  [bookingConfig.INPUT_END_TIME]: '',
+};
+
+export default initialValues;
