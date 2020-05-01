3eaec3c201d416cf9c208539e5fdffbfe7f58e66
Muzikayise Flynn Buthelezi (zuluCoda) <muzi@mfbproject.co.za>
2020-05-01 19:48:00 +0200

    styling: layout form and table

---
+++ src/components/layout/form/index.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
+import Box from '@material-ui/core/Box';
+import Paper from '@material-ui/core/Paper';
+import { COLOURS, SHADOWS } from '../../../config';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: 'flex',
+    minHeight: '90vh',
+    backgroundColor: COLOURS.DARK_BLUE,
+  },
+  paper: {
+    padding: theme.spacing(4, 3),
+    [theme.breakpoints.up('md')]: {
+      padding: theme.spacing(8, 6),
+    },
+    backgroundColor: COLOURS.PURPLE,
+    boxShadow: SHADOWS.BLACK,
+    color: 'white',
+  },
+}));
+
+const FormLayout = (props) => {
+  const classes = useStyles();
+  const { children } = props;
+
+  return (
+    <div className={classes.root}>
+      <Container maxWidth="sm">
+        <Box mt={7} mb={12}>
+          <Paper className={classes.paper}>{children}</Paper>
+        </Box>
+      </Container>
+    </div>
+  );
+};
+
+export default FormLayout;
--- src/components/layout/table/index.jsx
+++ src/components/layout/table/index.jsx
@@ -3,29 +3,32 @@ import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
 import Box from '@material-ui/core/Box';
 import Paper from '@material-ui/core/Paper';
+import { COLOURS, SHADOWS } from '../../../config';
 
 const useStyles = makeStyles((theme) => ({
   root: {
     display: 'flex',
-    backgroundImage: 'url(/assets/images/bg.jpg)',
-    backgroundRepeat: 'repeat',
     minHeight: '90vh',
+    backgroundColor: COLOURS.DARK_BLUE,
   },
   paper: {
     padding: theme.spacing(4, 3),
     [theme.breakpoints.up('md')]: {
       padding: theme.spacing(8, 6),
     },
+    backgroundColor: COLOURS.PURPLE,
+    boxShadow: SHADOWS.BLACK,
+    color: 'white',
   },
 }));
 
-const FormLayout = (props) => {
+const TableLayout = (props) => {
   const classes = useStyles();
   const { children } = props;
 
   return (
     <div className={classes.root}>
-      <Container maxWidth="sm">
+      <Container maxWidth="md">
         <Box mt={7} mb={12}>
           <Paper className={classes.paper}>{children}</Paper>
         </Box>
@@ -34,4 +37,4 @@ const FormLayout = (props) => {
   );
 };
 
-export default FormLayout;
+export default TableLayout;
