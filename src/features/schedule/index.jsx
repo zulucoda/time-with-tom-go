3550d5abb4d2bca04744c85f785a2727b9f7b60d
Muzikayise Flynn Buthelezi (zuluCoda) <muzi@mfbproject.co.za>
2020-05-01 19:51:14 +0200

    styling: adding schedule list and alert

--- src/features/schedule/index.jsx
+++ src/features/schedule/index.jsx
@@ -1,15 +1,32 @@
-import React from 'react';
-import styled from 'styled-components';
-
-const Wrapper = styled.div`
-  padding: 0 1em 0 1em;
-`;
+import React, { useEffect, useState } from 'react';
+import Typography from '@material-ui/core/Typography';
+import { fakeGet } from '../../fake-services';
+import AlertError from '../../components/alerts/error';
+import TableLayout from '../../components/layout/table';
+import ScheduleTable from './table';
 
 const Schedule = () => {
+  const [scheduleList, setScheduleList] = useState([]);
+  useEffect(() => {
+    const data = fakeGet();
+    setScheduleList(data);
+  }, []);
+
+  if (scheduleList.length === 0) {
+    return <AlertError message="There is no schedule data." />;
+  }
+
   return (
-    <Wrapper>
-      <h2>Schedule</h2>
-    </Wrapper>
+    <TableLayout>
+      <Typography variant="h3" gutterBottom marked="center" align="center">
+        Schedule
+      </Typography>
+      {scheduleList.length === 0 ? (
+        <AlertError message="There is no schedule data." />
+      ) : (
+        <ScheduleTable scheduleList={scheduleList} />
+      )}
+    </TableLayout>
   );
 };
 
