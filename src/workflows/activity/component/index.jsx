04ab57dda62a07070710f2864d8436206c861afe
Muzikayise Flynn Buthelezi (zuluCoda) <muzi@mfbproject.co.za>
2020-05-09 20:17:22 +0200

    booking-request-styling: refactor to use select field

--- src/workflows/activity/component/index.jsx
+++ src/workflows/activity/component/index.jsx
@@ -1,34 +1,15 @@
 import React from 'react';
 import activityConfig from '../constants';
-import { useField } from 'formik';
+import SelectField from '../../../components/fields/select';
 
 const Activity = () => {
-  const [, meta, helpers] = useField({
-    name: activityConfig.SELECT_NAME,
-    value: activityConfig.ACTIVITY_VALUE_NONE,
-  });
-
-  const { value } = meta;
-  const { setValue } = helpers;
-
-  const onChange = (e) => {
-    e.preventDefault();
-    setValue(e.target.value);
-  };
-
   return (
     <div>
-      <select
+      <SelectField
+        label={activityConfig.SELECT_NAME}
         name={activityConfig.SELECT_NAME}
-        value={value}
-        onChange={onChange}
-      >
-        {activityConfig.ACTIVITY_LIST.map(({ value, title }, index) => (
-          <option key={index} value={value}>
-            {title}
-          </option>
-        ))}
-      </select>
+        list={activityConfig.ACTIVITY_LIST}
+      />
     </div>
   );
 };
