3320b0fd7cebdd97245f68657b5ce4d75660b7db
Muzikayise Flynn Buthelezi (zuluCoda) <muzi@mfbproject.co.za>
2020-05-09 23:35:45 +0200

    booking-request-styling: text fields

--- src/components/fields/text/index.jsx
+++ src/components/fields/text/index.jsx
@@ -2,6 +2,42 @@ import React from 'react';
 import { useField } from 'formik';
 import ErrorField from '../error';
 import { string } from 'prop-types';
+import TextFieldUi from '@material-ui/core/TextField';
+import styled from 'styled-components';
+import { COLOURS } from '../../../config';
+
+const Container = styled.div`
+  width: 100%;
+
+  .MuiFormControl-root {
+    label {
+      color: ${COLOURS.LIGHT_GRAY};
+      font-weight: bold;
+      text-transform: capitalize;
+      &.Mui-focused {
+        color: ${COLOURS.GREEN};
+      }
+    }
+    &.MuiTextField-root {
+      width: 100%;
+      .MuiInput-root {
+        color: ${COLOURS.LIGHT_GRAY};
+        &.MuiInput-underline {
+          border-color: ${COLOURS.LIGHT_GRAY};
+          &:hover {
+            border-color: ${COLOURS.LIGHT_GRAY};
+          }
+          &::before {
+            border-color: ${COLOURS.LIGHT_GRAY};
+          }
+          &::after {
+            border-color: ${COLOURS.GREEN};
+          }
+        }
+      }
+    }
+  }
+`;
 
 /**
  * Formik Text Field
@@ -12,11 +48,10 @@ import { string } from 'prop-types';
 const TextField = ({ label, ...props }) => {
   const [field, meta, helpers] = useField(props);
   return (
-    <>
-      <label>{label}</label>
-      <input {...field} {...props} />
+    <Container>
+      <TextFieldUi label={label} {...field} {...props} />
       <ErrorField meta={meta} />
-    </>
+    </Container>
   );
 };
 
