5e04e6912707ba27e0a6dab2ad6121fd2ff8d78e
Muzikayise Flynn Buthelezi (zuluCoda) <muzi@mfbproject.co.za>
2020-05-10 01:41:00 +0200

    booking-request-styling: updating booking request tests

--- src/features/booking-request/formik/index.test.jsx
+++ src/features/booking-request/formik/index.test.jsx
@@ -1,13 +1,18 @@
 import { render } from '@testing-library/react';
 import React from 'react';
 import BookingRequestFormik from '.';
-import { ReactRouterWrapperTestUtil } from '../../../test-util';
+import {
+  DateAndTimePickerProviderWrapperTestUtil,
+  ReactRouterWrapperTestUtil,
+} from '../../../test-util';
 
 describe('Features - Booking Request - Form - Unit Test', function () {
   test('does it render', () => {
     const { container } = render(
       <ReactRouterWrapperTestUtil>
-        <BookingRequestFormik />
+        <DateAndTimePickerProviderWrapperTestUtil>
+          <BookingRequestFormik />
+        </DateAndTimePickerProviderWrapperTestUtil>
       </ReactRouterWrapperTestUtil>,
     );
     expect(container).toMatchSnapshot();
