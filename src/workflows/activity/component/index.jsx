ecb8714c7e1f77d81df7d3b7b5ba58272b0d5c9a
Muzikayise Flynn Buthelezi (zuluCoda) <muzi@mfbproject.co.za>
2020-05-09 23:14:52 +0200

    booking-request-styling: activity

--- src/workflows/activity/component/index.jsx
+++ src/workflows/activity/component/index.jsx
@@ -1,16 +1,21 @@
 import React from 'react';
 import activityConfig from '../constants';
 import SelectField from '../../../components/fields/select';
+import styled from 'styled-components';
+
+const Container = styled.div`
+  margin-bottom: 1rem;
+`;
 
 const Activity = () => {
   return (
-    <div>
+    <Container>
       <SelectField
         label={activityConfig.SELECT_NAME}
         name={activityConfig.SELECT_NAME}
         list={activityConfig.ACTIVITY_LIST}
       />
-    </div>
+    </Container>
   );
 };
 
