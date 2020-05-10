3e502ce69dfb8ab2df8141d7496878fd29970fa2
Muzikayise Flynn Buthelezi (zuluCoda) <muzi@mfbproject.co.za>
2020-05-10 02:07:07 +0200

    booking-request-styling: styling booking workflow

--- src/workflows/booking/component/index.jsx
+++ src/workflows/booking/component/index.jsx
@@ -1,16 +1,19 @@
 import React from 'react';
-import TextField from '../../../components/fields/text';
 import bookingConfig from '../constants';
 import DateField from '../../../components/fields/date';
 import TimeField from '../../../components/fields/time';
+import styled from 'styled-components';
+
+const Container = styled.div`
+  width: 100%;
+  margin-bottom: 1rem;
+  display: flex;
+  flex-direction: row;
+`;
 
 const Booking = () => {
   return (
-    <div>
-      <DateField
-        label={bookingConfig.INPUT_DATE}
-        name={bookingConfig.INPUT_DATE}
-      />
+    <Container>
       <TimeField
         label={bookingConfig.INPUT_START_TIME}
         name={bookingConfig.INPUT_START_TIME}
@@ -19,7 +22,11 @@ const Booking = () => {
         label={bookingConfig.INPUT_END_TIME}
         name={bookingConfig.INPUT_END_TIME}
       />
-    </div>
+      <DateField
+        label={bookingConfig.INPUT_DATE}
+        name={bookingConfig.INPUT_DATE}
+      />
+    </Container>
   );
 };
 
