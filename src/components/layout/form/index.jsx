89125792a7e1044067b36c74c9e51a681aff7a65
Muzikayise Flynn Buthelezi (zuluCoda) <muzi@mfbproject.co.za>
2020-05-14 22:11:13 +0200

    booking-request-styling: update submit button styles and remove debug

--- src/components/layout/form/index.jsx
+++ src/components/layout/form/index.jsx
@@ -3,7 +3,7 @@ import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
 import Box from '@material-ui/core/Box';
 import Paper from '@material-ui/core/Paper';
-import { COLOURS, SHADOWS } from '../../../config';
+import { COLOURS, GRADIENTS, SHADOWS } from '../../../config';
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -16,8 +16,8 @@ const useStyles = makeStyles((theme) => ({
     [theme.breakpoints.up('md')]: {
       padding: theme.spacing(8, 6),
     },
-    backgroundColor: COLOURS.PURPLE,
-    boxShadow: SHADOWS.BLACK,
+    background: GRADIENTS.BLUE,
+    boxShadow: SHADOWS.BLUE,
     color: 'white',
   },
 }));
--- src/features/booking-request/form/index.jsx
+++ src/features/booking-request/form/index.jsx
@@ -1,26 +1,44 @@
-import { Form, useFormikContext } from 'formik';
+import { Form } from 'formik';
 import React from 'react';
 import { array } from 'prop-types';
+import styled from 'styled-components';
+import { COLOURS, GRADIENTS } from '../../../config';
+
+const Container = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+`;
+
+const SubmitButton = styled.button`
+  background: ${GRADIENTS.BLUE};
+  color: ${COLOURS.LIGHT_GRAY};
+
+  font-size: 1em;
+  margin-top: 1rem;
+  padding: 0.25em 1em;
+  border: 2px solid ${COLOURS.GREEN};
+  border-radius: 3px;
+  cursor: pointer;
+
+  &:hover {
+    background: ${COLOURS.GREEN};
+    color: ${COLOURS.DARK_BLUE};
+  }
+  &:target {
+    border: 0;
+  }
+`;
 
 const BookingRequestForm = ({ workflows }) => {
-  const { values, errors } = useFormikContext();
-
   return (
     <Form>
-      {workflows.map((Workflow, index) => (
-        <Workflow.Component key={index} />
-      ))}
-
-      <pre>
-        debug value
-        {JSON.stringify(values, null, 3)}
-      </pre>
-      <pre>
-        debug errors
-        {JSON.stringify(errors, null, 3)}
-      </pre>
-
-      <button type="submit"> Submit </button>
+      <Container>
+        {workflows.map((Workflow, index) => (
+          <Workflow.Component key={index} />
+        ))}
+        <SubmitButton type="submit"> Submit </SubmitButton>
+      </Container>
     </Form>
   );
 };
