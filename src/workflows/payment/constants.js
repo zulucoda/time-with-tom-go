335b81052321cb823d9d4edb8dc9bd58b4130421
Muzikayise Flynn Buthelezi (zuluCoda) <muzi@mfbproject.co.za>
2020-05-25 23:11:15 +0200

    payment-workflow: adding payment worflow

---
+++ src/workflows/payment/constants.js
@@ -0,0 +1,35 @@
+/**
+ * credit card name
+ * @type {string}
+ */
+const INPUT_CC_NAME = 'ccName';
+/**
+ * credit card number
+ * @type {string}
+ */
+const INPUT_CC_NUMBER = 'ccNumber';
+/**
+ * credit card cvc
+ * @type {string}
+ */
+const INPUT_CC_CVC = 'ccCvc';
+
+/**
+ * credit card expiry month
+ * @type {string}
+ */
+const INPUT_CC_EXPIRY_MONTH = 'ccExpiryMonth';
+
+/**
+ * credit card expiry year
+ * @type {string}
+ */
+const INPUT_CC_EXPIRY_YEAR = 'ccExpiryYear';
+
+export default {
+  INPUT_CC_NAME,
+  INPUT_CC_NUMBER,
+  INPUT_CC_CVC,
+  INPUT_CC_EXPIRY_MONTH,
+  INPUT_CC_EXPIRY_YEAR,
+};
