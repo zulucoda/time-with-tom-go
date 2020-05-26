52a90c76a0d35b06d35583012607ae2091b15988
Muzikayise Flynn Buthelezi (zuluCoda) <muzi@mfbproject.co.za>
2020-05-26 21:54:42 +0200

    update-booking-request-for-some-paid: active description

--- src/workflows/payment/active/index.js
+++ src/workflows/payment/active/index.js
@@ -1,3 +1,14 @@
+/**
+ *
+ * The isActive acts as the business logic section. In this section we may use whatever logic or data to
+ * verify whether our form should be active. This will be used by formik to check whether it needs to do
+ * the following for the workflow:
+ *  - display the workflow component with its fields
+ *  - do the validation
+ *  - get the payload
+ *
+ * @return {boolean}
+ */
 const isActive = () => true;
 
 export default isActive;
