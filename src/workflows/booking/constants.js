033c4b06a1974d671b9f7c043b0ee98e4c6eaddb
Muzikayise Flynn Buthelezi (zuluCoda) <muzi@mfbproject.co.za>
2020-05-26 22:52:47 +0200

    paid-activity: updating titles for bookign worfklow

--- src/workflows/booking/constants.js
+++ src/workflows/booking/constants.js
@@ -3,19 +3,25 @@
  * @type {string}
  */
 const INPUT_DATE = 'date';
+const TITLE_DATE = 'Date';
 /**
  * startTime
  * @type {string}
  */
 const INPUT_START_TIME = 'startTime';
+const TITLE_START_TIME = 'Start Time';
 /**
  * endTime
  * @type {string}
  */
 const INPUT_END_TIME = 'endTime';
+const TITLE_END_TIME = 'End Time';
 
 export default {
   INPUT_DATE,
+  TITLE_DATE,
   INPUT_START_TIME,
+  TITLE_START_TIME,
   INPUT_END_TIME,
+  TITLE_END_TIME,
 };
